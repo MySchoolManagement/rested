b0f516ff40532c288417cbc271091c253b02f095
Iain Mckay <me@iainmckay.co.uk>
2015-07-03 11:49:22 +0200

    Removed old methods for dealing with forms in responses

--- src/Response.php
+++ src/Response.php
@@ -7,54 +7,6 @@ use Symfony\Component\Form\FormInterface;
 abstract class Response
 {
 
-    /**
-     * List of high-level messages (warnings, errors) about the result of this endpoint.
-     *
-     * @var array
-     */
-    public $messages = [];
-
-    /**
-     * Adds form data to the result.
-     */
-    public function addForm(FormInterface $form)
-    {
-        $this->validation = [
-            'is_valid' => $form->isValid(),
-            'errors'   => []
-        ];
-
-        $this->getErrorMessages($form);
-    }
-
-    public function addMessage($type, $message)
-    {
-        $this->messages[] = [
-            'type'    => $type,
-            'message' => $message
-        ];
-    }
-
-    /**
-     * @param \Symfony\Component\Form\Form $form
-     *
-     * @return array
-     */
-    private function getErrorMessages(FormInterface $form)
-    {
-        if ($form->count() > 0) {
-            foreach ($form->all() as $child) {
-                if ($child->isValid() == false) {
-                    $this->getErrorMessages($child);
-                }
-            }
-        } else {
-            foreach ($form->getErrors() as $error) {
-                $this->validation['errors'][$form->getName()] = $error->getMessage();
-            }
-        }
-    }
-
     public abstract function toJson(AbstractResource $resource);
 
     /**
