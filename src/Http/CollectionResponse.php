d03201cb43f84c02a48fe2bec59702df522880f0
Iain Mckay <me@iainmckay.co.uk>
2015-07-10 16:00:37 +0200

    Forced _embedded.items for collections

--- src/Http/CollectionResponse.php
+++ src/Http/CollectionResponse.php
@@ -20,4 +20,20 @@ class CollectionResponse extends Response
         $this->setResource('items', $items);
         $this->addActions($resource, [ActionDefinition::TYPE_CREATE]);
     }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function asJson($pretty = false, $encode = true)
+    {
+        $data = parent::asJson($pretty, false);
+
+        // enforce _embedded.items for collections
+        if ((array_key_exists('_embedded', $data) === false)
+            || (array_key_exists('items', $data['_embedded']) === false)) {
+            $data['_embedded']['items'] = [];
+        }
+
+        return json_encode($data);
+    }
 }
